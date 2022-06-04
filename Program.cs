ea41f2cd3924ba309221da7ac8c0fe78eef31f2f
Kat <98545016+Katenski@users.noreply.github.com>
2022-06-04 15:27:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ! Actually Works
+            Console.WriteLine("Welcome");
+            Console.Write("Insert full path of a file to delete: ");
+            string FileToDelete = Convert.ToString(Console.ReadLine());
+            File.Delete(FileToDelete);
+        }
+    }
+}
